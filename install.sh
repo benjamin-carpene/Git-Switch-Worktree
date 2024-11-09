0c19dfe4ffd6c0ae8c5de254d565aa04924b1118
benjamin-carpene <benjamincarpene@gmail.com>
2024-11-09 22:23:31 +0100

    feat: Added an alias recomendation in the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 
 if [ -n $HOME ]; then
+    type gsw &>/dev/null && set_alias_msg='true' # if something with that name already exist
+
     # Installs the script
     local_install_dir="$HOME/.local/gsw"
     mkdir -p "$local_install_dir"
@@ -16,6 +18,12 @@ if [ -n $HOME ]; then
         echo "You may not have $local_bin_dir in your PATH variable, which is necessary for gsw."
         echo "Type \`echo 'PATH=\$PATH:$local_bin_dir' >> $HOME/.bashrc\` in order to add it to your path."
     fi
+
+    if [ "$set_alias_msg" == 'true' ]; then
+        echo "You may want to set an alias for gsw, as you already have a command named gsw."
+        echo "Type \`echo 'alias gsw=$local_bin_dir/gsw' >> $HOME/.bashrc\` to add it as an alias."
+    fi
+
 else
     echo "User has no HOME folder"
     exit 1
